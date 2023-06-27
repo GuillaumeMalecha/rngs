59fa737725b0cbb53220b98498e5a92ed706fbfa
GuillaumeMalecha <guillaume.malecha@gmail.com>
2023-06-27 16:36:58 +0200

    mise en place theme

---
+++ public/assets/app.js
@@ -0,0 +1,40 @@
+/*
+ * Welcome to your app's main JavaScript file!
+ *
+ * We recommend including the built version of this JavaScript file
+ * (and its CSS file) in your base layout (base.html.twig).
+ */
+
+// any CSS you import will output into a single css file (app.css in this case)
+import 'bootstrap/scss/bootstrap.scss';
+import './css/plugins.css';
+import './css/nav.css';
+import './css/styles.css';
+import './css/colors.css';
+import './css/slick.scss';
+import './css/slick-theme.scss';
+
+//import './js/jquery.min';
+import $ from 'jquery';
+global.$ = global.jQuery = $
+
+
+
+import './js/circleMagic.min';
+import './js/popper.min';
+require('bootstrap');
+import './js/rangeslider';
+import './js/select2.min';
+import './js/aos';
+//import './js/owl.carousel.min';
+require('owl.carousel');
+import './js/jquery.magnific-popup.min';
+import './js/slick';
+import './js/slider-bg';
+import './js/lightbox';
+import './js/imagesloaded';
+import './js/isotope.min';
+import './js/coreNavigation';
+import './js/custom';
+
+// Need jQuery? Install it with "yarn add jquery", then uncomment to import it.
